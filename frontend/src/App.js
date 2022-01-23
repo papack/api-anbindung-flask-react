e44e7e14eae80dfb8f9a5edd03d800a91e23a613
Matthias Steiner <m@kjpjm.de>
2022-01-23 01:31:31 +0100

    inital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,88 @@
+import "./App.css";
+import { useImageUpload } from "./components/hooks";
+const preventDefaultBrowserBehavior = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+};
+
+function App() {
+  //get vars from hook
+  const {
+    handleImageUpload,
+    dataAvailable,
+    imagePaths,
+    price,
+    vintedURL,
+    vindetUsername,
+    isLoading,
+    errorMassage,
+  } = useImageUpload();
+
+  return (
+    <>
+      {/* Example File Drop / Einfach mit eurem ersetzen */}
+      <div
+        style={{
+          width: "500px",
+          height: "500px",
+          backgroundColor: isLoading ? "gray" : "green",
+        }}
+        onDrag={preventDefaultBrowserBehavior}
+        onDragStart={preventDefaultBrowserBehavior}
+        onDragEnd={preventDefaultBrowserBehavior}
+        onDragOver={preventDefaultBrowserBehavior}
+        onDragEnter={preventDefaultBrowserBehavior}
+        onDragLeave={preventDefaultBrowserBehavior}
+        onDrop={(e) => {
+          preventDefaultBrowserBehavior(e);
+
+          //get the file
+          const file = e.dataTransfer.files[0];
+
+          //handle upload
+          handleImageUpload(file);
+        }}
+      />
+
+      {/* Show Error, if we have any */}
+      {errorMassage && (
+        <div style={{ backgroundColor: "red" }}>{errorMassage}</div>
+      )}
+
+      {/* Example how to use the data from hook */}
+      {dataAvailable && (
+        <div>
+          {imagePaths.map((imagePath) => (
+            <img
+              key={imagePath}
+              src={imagePath}
+              alt="uploaded"
+              style={{ width: "200px", height: "200px" }}
+            />
+          ))}
+
+          <ul>
+            <li>
+              isLoading:
+              {isLoading.toString()}
+            </li>
+            <li>
+              price:
+              {price}
+            </li>
+            <li>
+              vintedURL:
+              {vintedURL}
+            </li>
+            <li>
+              vindetUsername:
+              {vindetUsername}
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/hooks/useImageUpload/useImageUpload.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useState } from "react";
+
+export const useImageUpload = () => {
+  const [imagePaths, setImagePaths] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [vintedURL, setVintedURL] = useState("");
+  const [vindetUsername, setVindetUsername] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMassage, setErrorMassage] = useState("");
+  const [dataAvailable, setDataAvailable] = useState(false); // wurden Daten abgerufen? sollen dem user eine Rückmeldung angezeigt werden?
+
+  const handleImageUpload = async (file) => {
+    //prepare request / set loading / remove previous error message
+    setIsLoading(true);
+    setErrorMassage("");
+
+    //upload file to server as multipart/form-data
+    const formData = new FormData();
+    formData.append("imagefile", file);
+
+    //send the file to the server
+    try {
+      const {
+        data: { imagePaths, price, vintedURL, vintedUsername },
+      } = await axios.post(
+        "http://localhost:5000/api/v1/process-image",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      //set the data to state
+      setPrice(price);
+      setImagePaths(imagePaths);
+      setVintedURL(vintedURL);
+      setVindetUsername(vintedUsername);
+      setIsLoading(false);
+      setDataAvailable(true);
+    } catch ({ message }) {
+      setErrorMassage(message);
+      setPrice(0);
+      setImagePaths([]);
+      setVintedURL("");
+      setVindetUsername("");
+      setIsLoading(false);
+      setDataAvailable(false);
+    }
+  };
+
+  return {
+    handleImageUpload,
+    dataAvailable,
+    imagePaths,
+    price,
+    vintedURL,
+    vindetUsername,
+    isLoading,
+    errorMassage,
+  };
+};
